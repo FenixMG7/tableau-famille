2268f4981d17dcc27a2634b0b9c46f867afc7d92
FenixMG7 <milangrangeasse@gmail.com>
2026-02-08 22:55:25 +0100

    Add Firebase configuration file
    This file contains the Firebase configuration settings.

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Ce fichier sera vide sur GitHub pour la sécurité
+export const firebaseConfig = {
+  apiKey: "REMPLACER_API_KEY",
+  authDomain: "tableaufamille-eb08b.firebaseapp.com",
+  projectId: "tableaufamille-eb08b",
+  storageBucket: "tableaufamille-eb08b.appspot.com",
+  messagingSenderId: "739527539477",
+  appId: "REMPLACER_APP_ID"
+};
